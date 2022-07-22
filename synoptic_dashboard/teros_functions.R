e6ec6227f105e1d244110e73f4c915fc5d8deba2
peterregier <peter.regier@pnnl.gov>
2022-07-21 18:05:25 -0700

    Update teros_functions.R

--- synoptic_dashboard/teros_functions.R
+++ synoptic_dashboard/teros_functions.R
@@ -12,31 +12,39 @@ fileread <- function(data) {
   
   # Lines 1, 3, and 4 of the TEROS data files contain sensor metadata that we want to remove
   # Read the data files into a string vector, remove those lines, and then pass to read.csv()
-  teros_primitive <- readLines("tempfile.dat")[-c(1, 3, 4)] #temp file goes here
+  teros_primitive <- read_lines("tempfile.dat") #temp file goes here
+  teros_primitive <- teros_primitive[-3:-4]
   
   unlink("tempfile.dat")
-  
-  textConnection(teros_primitive) %>%
-    read.csv(check.names = FALSE, na.strings = "NAN", stringsAsFactors = FALSE) %>%
-    as_tibble() %>%
+
+  #textConnection(teros_primitive) %>%
+    read_csv(I(teros_primitive), 
+             skip = 1,
+             #check.names = FALSE, 
+             na = "NAN",
+             #na.strings = "NAN", 
+             #stringsAsFactors = FALSE, 
+             col_types = "Tddcddddddddddddddddddd") %>%
+    as_tibble() -> b #%>%
+
     # Reshape the data frame to one observation per row
-    gather(channel, value, -TIMESTAMP, -RECORD, -Statname) %>%
-    filter(!is.na(value)) %>%
-    # Pull data logger ID out of statname
-    separate(Statname, into = c("Inst", "Logger"), sep = "_" ) %>%
-    # Parse the data logger number, channel number, and variable number out of the
-    # Statname and Channel columns
-    mutate(Logger = as.integer(Logger, fixed = TRUE),
-           TIMESTAMP = ymd_hms(TIMESTAMP, tz = "EST")) %>%
-    select(-Inst) %>%  # unneeded
-    # Next, parse channel into the data logger channel and variable number
-    separate(channel, into = c("Data_Table_ID", "variable"), sep = ",") %>%
-    mutate(Data_Table_ID = as.integer(gsub("Teros(", "", Data_Table_ID, fixed = TRUE)),
-           variable = as.integer(gsub(")", "", variable, fixed = TRUE)),
-           # Give them sensible names
-           variable = case_when(variable == 1 ~ "VWC",
-                                variable == 2 ~ "TSOIL",
-                                variable == 3 ~ "EC"))
+    # gather(channel, value, -TIMESTAMP, -RECORD, -Statname) %>%
+    # filter(!is.na(value)) %>%
+    # # Pull data logger ID out of statname
+    # separate(Statname, into = c("Inst", "Logger"), sep = "_" ) %>%
+    # # Parse the data logger number, channel number, and variable number out of the
+    # # Statname and Channel columns
+    # mutate(Logger = as.integer(Logger, fixed = TRUE),
+    #        TIMESTAMP = ymd_hms(TIMESTAMP, tz = "EST")) %>%
+    # select(-Inst) %>%  # unneeded
+    # # Next, parse channel into the data logger channel and variable number
+    # separate(channel, into = c("Data_Table_ID", "variable"), sep = ",") %>%
+    # mutate(Data_Table_ID = as.integer(gsub("Teros(", "", Data_Table_ID, fixed = TRUE)),
+    #        variable = as.integer(gsub(")", "", variable, fixed = TRUE)),
+    #        # Give them sensible names
+    #        variable = case_when(variable == 1 ~ "VWC",
+    #                             variable == 2 ~ "TSOIL",
+    #                             variable == 3 ~ "EC"))
 }
 
 
