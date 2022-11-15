81f4b7eb9ea177c3bab2d29bdda7804010922f23
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-11-15 18:26:33 -0500

    Move processed files to done folder

---
+++ portage/Raw/done/README.md
@@ -0,0 +1,3 @@
+# Raw/done
+
+This folder holds raw data that are done (processed to L0).
--- portage/raw_to_L0.R
+++ portage/raw_to_L0.R
@@ -4,6 +4,7 @@ library(quarto)
 
 RAW <- "Raw/"
 L0 <- "L0/"
+RAW_DONE <- file.path(RAW, "done/")
 
 # Move files from raw to L0
 outfile <- paste0("raw_to_L0_", format(Sys.time(), "%Y%m%d%H%M%S"), ".html")
@@ -11,5 +12,6 @@ outfile_fq <- file.path("portage", "L0", outfile)
 quarto::quarto_render("portage/raw_to_L0.qmd",
                       output_file = outfile_fq,
                       execute_params = list(raw = RAW,
+                                            raw_done = RAW_DONE,
                                             L0 = L0,
                                             outfile = outfile))
