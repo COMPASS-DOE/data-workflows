b7032d7012984d414881df4683157c9e6d0770bb
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-01-20 14:05:17 -0500

    Restructure file structure; bring complete site data together in L1a step
    L1_normalize files are sorted into folders, files by table
    L1a files are sorted into folders, files by site/year/month

--- portage/helpers.R
+++ portage/helpers.R
@@ -45,41 +45,42 @@ write_to_folders <- function(x, root_dir, data_level, site, logger, table, quiet
     years <- year(x$TIMESTAMP)
     months <- sprintf("%02i", month(x$TIMESTAMP)) # add leading zero if needed
 
-    for(y in unique(years)) {
-        for(m in unique(months)) {
+        for(y in unique(years)) {
+            for(m in unique(months)) {
 
-            # Construct folder name and create it if necessary
-            folder <- file.path(root_dir, paste(y, m, site, sep = "_"))
-            if(!dir.exists(folder)) {
-                # Create folder
-                if(!quiet) message("Creating ", basename(folder))
-                if(!dir.create(folder)) {
-                    stop("dir.create returned an error")
+                # Construct folder name (<year>_<month>_<site>) and create if necessary
+                folder <- file.path(root_dir, paste(y, m, site, sep = "_"))
+                if(!dir.exists(folder)) {
+                    # Create folder
+                    if(!quiet) message("Creating ", basename(folder))
+                    if(!dir.create(folder)) {
+                        stop("dir.create returned an error")
+                    }
                 }
+
+                # Isolate the data to write
+                dat <- x[y == years & m == months,]
+                stopifnot(nrow(dat) > 0) # this shouldn't happen
+
+                # Construct filename and write the data
+                if(data_level == "L1_normalize") {
+                    # L1_normalize data: <logger>_<table>_<year>_<month>
+                    filename <- paste0(paste(logger, table, y, m, sep = "_"), ".csv")
+                } else if(data_level %in% c("L1b", "L1a")) {
+                    # L1a and L1b data: <site>_<year>_<month>
+                    filename <- paste0(paste(site, y, m, sep = "_"), ".csv")
+                } else {
+                    stop("Unkown data_level ", data_level)
+                }
+                if(!quiet) message("Writing ", nrow(dat), "/", nrow(x), " rows of data to ",
+                                   basename(folder), "/", filename)
+
+                fn <- file.path(folder, filename)
+                if(file.exists(fn)) message("NOTE: overwriting existing file")
+                readr::write_csv(dat, fn)
             }
-
-            # Isolate the data to write
-            dat <- x[y == years & m == months,]
-            stopifnot(nrow(dat) > 0) # this shouldn't happen
-
-            # Construct filename and write the data
-            if(data_level == "L1a") {
-                # L1a data: filename is <site>_<year>_<month>
-                filename <- paste0(paste(logger, table, y, m, sep = "_"), ".csv")
-            } else if(data_level == "L1b") {
-                # L1b data: filename is <site>_<year>_<month>
-                filename <- paste0(paste(site, y, m, sep = "_"), ".csv")
-            } else {
-                stop("Unkown data_level ", data_level)
-            }
-            if(!quiet) message("Writing ", nrow(dat), "/", nrow(x), " rows of data to ",
-                               basename(folder), "/", filename)
-
-            fn <- file.path(folder, filename)
-            if(file.exists(fn)) message("NOTE: overwriting existing file")
-            readr::write_csv(dat, fn)
-        }
     }
+
 }
 
 
