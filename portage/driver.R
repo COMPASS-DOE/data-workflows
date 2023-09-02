ef878c3d5c1a6121dd74e6213d96f72464a4f3bf
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-09-02 15:41:10 -0400

    What is going on

--- portage/driver.R
+++ portage/driver.R
@@ -43,6 +43,8 @@ quarto_render("L0.qmd",
                                     logfile = LOGFILE))
 copy_output("L0.html", outfile)
 
+list_directories(list("data_TEST/L0/", "data_TEST/L1_normalize/", "data_TEST/L1a/"))
+
 # 'Normalize' L0 data -------------------------------------------
 # Reshaped to long form and matched with design_link info
 # This is an intermediate step, not exposed to data users
@@ -60,6 +62,7 @@ quarto_render("L1_normalize.qmd",
                                     logfile = LOGFILE))
 copy_output("L1_normalize.html", outfile)
 
+list_directories(list("data_TEST/L0/", "data_TEST/L1_normalize/", "data_TEST/L1a/"))
 
 # Construct L1a data --------------------------------------------
 # Unit conversion and bounds checks performed
@@ -81,6 +84,7 @@ quarto_render("L1a.qmd",
                                     logfile = LOGFILE))
 copy_output("L1a.html", outfile)
 
+list_directories(list("data_TEST/L0/", "data_TEST/L1_normalize/", "data_TEST/L1a/"))
 
 # Manual QA/QC step ---------------------------------------------
 # A MarineGEO-like Shiny app, allowing technicians to
