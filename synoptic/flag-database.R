5c09903d4aebd1c0268036875bf6025f890f437d
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-11-22 10:57:55 -0500

    Basic flag functionality: add data, get site data, get ID data, delete ID data

--- synoptic/flag-database.R
+++ synoptic/flag-database.R
@@ -3,20 +3,78 @@
 
 library(DBI)
 
-FDB <- dbConnect(RSQLite::SQLite(), "flag-db.sqlite")
+#FDB <- dbConnect(RSQLite::SQLite(), "flag-db.sqlite")
+#dbDisconnect(FDB)
 
-flag_db_template <- data.frame(Timestamp = as.POSIXct(NA),
-                               ID = NA_character_,
-                               Type = NA_character_)
-if(!"flags" %in% dbListTables(FDB)) {
-    dbWriteTable(FDB, "flags", flag_db_template)
+
+# Add flag data to the database
+# Each site gets its own table, which is created if necessary
+fdb_add_flags <- function(site, flag_data, fdb = FDB) {
+    if(site %in% dbListTables(fdb)) {
+        rows <- dbAppendTable(fdb, site, flag_data)
+    } else {
+        message("Creating new site ", site)
+        dbWriteTable(fdb, site, flag_data)
+        rows <- nrow(flag_data)
+    }
+    message("Added ", rows, " rows")
 }
-dbDisconnect(FDB)
 
-
-fdb_add_flags <- function(flag_data, fdb = FDB) {
-    ra <- dbAppendTable(fdb, "flags", flag_data)
-    message("Added ", ra, " rows")
+# Get flag data for a single site
+# If there are no flags for the site, return NULL
+fdb_get_flags_site <- function(site, fdb = FDB) {
+    if(site %in% dbListTables(fdb)) {
+        dbReadTable(fdb, site)
+    } else {
+        messsage("No flags for site ", site)
+        NULL
+    }
 }
 
-test1 <- data.frame(Timestamp = rep(Sys.time(), 3), ID = 4:6, Type = letters[1:3])
+# Get flag data for one or more IDs in a site
+# Supplying an invalid site is an error
+fdb_get_flags_ids <- function(site, ids, fdb = FDB) {
+    # Using paste() is theoretically dangerous in this context (see
+    # https://rsqlite.r-dbi.org/articles/rsqlite#queries) but I couldn't
+    # get the table name to work as a parameter
+    dbGetQuery(fdb,
+               paste('SELECT * FROM', site, 'WHERE ID IN (?)'),
+               params = list(ids))
+}
+
+# Delete flag data for one or more IDs in a site
+# Supplying an invalid site is an error
+fdb_delete_ids <- function(site, ids, fdb = FDB) {
+    # Using paste() is theoretically dangerous in this context (see
+    # https://rsqlite.r-dbi.org/articles/rsqlite#queries) but I couldn't
+    # get the table name to work as a parameter
+    res <- dbSendStatement(fdb,
+               paste('DELETE FROM', site, 'WHERE ID IN (?)'),
+               params = list(ids))
+    message("Deleted ", dbGetRowsAffected(res), " rows")
+    dbClearResult(res)
+}
+
+
+
+# ============= Test code =============
+
+# Create test database
+test_db <- dbConnect(RSQLite::SQLite(), ":memory:")
+test1 <- data.frame(ID = 1:3, Flag_type = letters[1:3])
+fdb_add_flags("site1", test1, fdb = test_db)
+
+# Pull out data for an entire site (table)
+x <- fdb_get_flags_site("site1", fdb = test_db)
+stopifnot(identical(x, test1))
+
+# Search by Site-ID
+x <- fdb_get_flags_ids("site1", ids = 1:2, fdb = test_db)
+stopifnot(identical(x, subset(test1, ID %in% 1:2)))
+
+# Remove IDs
+fdb_delete_ids("site1", ids = 1, fdb = test_db)
+x <- fdb_get_flags_site("site1", fdb = test_db)
+stopifnot(!1 %in% x$ID)
+
+dbDisconnect(test_db)
