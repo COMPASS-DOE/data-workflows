b987f89a192ebbf953a125e23378c65067cff34f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-12-03 12:52:16 -0500

    Use "valid_through"; less ambiguous than "valid_until"

--- synoptic/helpers.R
+++ synoptic/helpers.R
@@ -267,7 +267,7 @@ list_directories <- function(dir_list, outfile = "", prefix = "",
 # The design links might not be stable over time; for example, if a tree
 # dies, its sensor might get reassigned to a new tree. In this case the
 # design_link table will have two entries, one for the old assignment and
-# one for the new. We know which one to use by the "valid_until" column,
+# one for the new. We know which one to use by the "valid_through" column,
 # which give a end date for a design link--or, most commonly, it will be
 # empty (NA) indicating that there is no end date.
 #
@@ -276,24 +276,24 @@ list_directories <- function(dir_list, outfile = "", prefix = "",
 # design links.
 #
 # This function uses the object (i.e. group identifier; typically, Logger+
-# Table+Loggernet_variable), timestamp, and valid_until timestamps to identify
+# Table+Loggernet_variable), timestamp, and valid_through timestamps to identify
 # which rows to keep (correct design_link assignment) and which to drop.
-valid_entries <- function(objects, times, valid_until) {
-    # Nothing to do if there are no valid_until entries
-    if(all(is.na(valid_until))) return(rep(TRUE, length(objects())))
+valid_entries <- function(objects, times, valid_through) {
+    # Nothing to do if there are no valid_through entries
+    if(all(is.na(valid_through))) return(rep(TRUE, length(objects())))
 
-    # Any NA valid_until entries apply into the far future
-    valid_until[is.na(valid_until)] <- ymd_hms("2999-12-31 11:59:00")
-    past_valid_time <- times > valid_until
+    # Any NA valid_through entries apply into the far future
+    valid_through[is.na(valid_through)] <- ymd_hms("2999-12-31 11:59:00")
+    past_valid_time <- times > valid_through
 
     # Create a data frame to aggregate and then merge, below
-    x <- data.frame(obj = objects, time = times, vu = valid_until)
-    # Compute the minimum valid_until entry for each object and time that is
-    # not past the valid_until point; this is the 'controlling' value
+    x <- data.frame(obj = objects, time = times, vu = valid_through)
+    # Compute the minimum valid_through entry for each object and time that is
+    # not past the valid_through point; this is the 'controlling' value
     y <- aggregate(vu ~ obj + time, data = x[!past_valid_time,], FUN = min)
     names(y)[3] <- "controlling"
 
-    # Figure out controlling valid_until for each object/time
+    # Figure out controlling valid_through for each object/time
     z <- merge(x, y, all.x = TRUE)
     # An NA controlling entry means there is none
     valids <- z$vu == z$controlling
@@ -307,14 +307,14 @@ test_data <- data.frame(obj = c(1, 1, 1, 2, 2, 2), time = c(1, 2, 3, 1, 2, 3))
 # Object 2 changes its design link after time 2
 test_dt <- data.frame(obj = c(1,2,2),
                       dl = c("A", "B", "C"),
-                      valid_until = c(NA, 2, NA))
+                      valid_through = c(NA, 2, NA))
 # Merge the 'data' with the 'design link table'
 x <- merge(test_data, test_dt)
 # Call valid_entries. It figures out that all the object 1 entries should be
 # retained, but 1 of 2 entries in each timestep should be dropped for object 2.
 # This is because there are two design_table entries for it (see above); the
 # first ends at time point 2, and the second is indefinite after that.
-valid_entries(x$obj, x$time, x$valid_until)
+valid_entries(x$obj, x$time, x$valid_through)
 
 # Test code for valid_entries
 
@@ -330,20 +330,20 @@ stopifnot(ret == c(FALSE, TRUE, FALSE, TRUE))
 # One object, shifts
 ret <- valid_entries(c(1, 1, 1, 1), c(2, 2, 3, 3), c(2, NA, 2, NA))
 stopifnot(ret == c(TRUE, FALSE, FALSE, TRUE))
-# One objects, shifts twice (valid_untils at 1 and 2)
+# One objects, shifts twice (valid_throughs at 1 and 2)
 ret <- valid_entries(objects = rep(1, 9),
                      times = c(1, 1, 1, 2, 2, 2, 3, 3, 3),
-                     valid_until = c(1, 2, NA, 1, 2, NA, 1, 2, NA))
+                     valid_through = c(1, 2, NA, 1, 2, NA, 1, 2, NA))
 stopifnot(ret == c(TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE))
 # Two objects, only one shifts
 ret <- valid_entries(objects = c(1, 1, 1, 2, 2, 2, 2, 2, 2),
                      times = c(1, 2, 3, 1, 1, 2, 2, 3, 3),
-                     valid_until = c(NA, NA, NA, 2, NA, 2, NA, 2, NA))
+                     valid_through = c(NA, NA, NA, 2, NA, 2, NA, 2, NA))
 stopifnot(ret == c(TRUE, TRUE, TRUE, # obj 1
                    TRUE, FALSE, TRUE, FALSE, FALSE, TRUE)) # obj 2
-# There's a valid_until but no new entry
+# There's a valid_through but no new entry
 ret <- valid_entries(objects = c(1, 1),
                      times = c(1, 2),
-                     valid_until = c(1, 1))
+                     valid_through = c(1, 1))
 stopifnot(ret == c(TRUE, FALSE))
 
