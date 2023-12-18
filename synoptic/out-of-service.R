4bf1265ea86ee29143dbf19e6966394f51627045
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-12-18 12:00:27 -0500

    Core oos() function and test code for it

--- synoptic/out-of-service.R
+++ synoptic/out-of-service.R
@@ -7,6 +7,9 @@ library(tidyr)
 
 troll <- read.csv("data_TEST/out-of-service/troll_maintenance.csv")
 
+# ===========================================================
+# This code will be in L1_normalize?
+
 # If no time_pulled given, assume 6 AM
 tp <- troll$Time_pulled
 tp[is.na(tp) | tp == ""] <- "06:00"
@@ -24,71 +27,79 @@ troll$oos_begin <- mdy(troll$Date_pulled, tz = "EST") + hm(tp)
 troll$oos_end <- mdy(dr, tz = "EST") + hm(tr)
 # Per Peter R., we throw out all data for 24 hours after replacement
 troll$oos_end <- troll$oos_end + 60 * 60 * 24
+# ===========================================================
 
 
 
-troll_oos <- troll
+# We pass an oos_df data frame to oos()
+# This is a table of out-of-service windows
+# It MUST have `oos_begin` and `oos_end` timestamps; optionally,
+# it can have other columns that must be matched as well
+# (e.g., site, plot, etc)
 
-# oos (out of service)
-# Flag observations that match out-of-service entries
-# oos_dir: out-of-service file directory
+# The second thing we pass is the observation data frame
 
-# We'd like L1_normalize to pass oos_dir, timestamps, site, and
-# design links, and be ignorant of everything else
+# This function returns a logical vector that becomes F_OOS
 
-# We will hardcode things for AquaTroll, etc.?
+oos <- function(oos_df, data_df) {
 
-# Probably we will read in Aquatroll oos file in L1_normalize,
-# do what we need to do to compute begin_oos and end_oos timestamps,
-# and then pass in to this function?
-
-oos <- function(data_design_links, data_ts,
-                oos_sites, oos_plots, oos_dl_pattern,
-                oos_out_ts, oos_in_ts) {
-
-    # The oos_table has
-    #   sensor, sites (vector), plots (vector),
-    #   which_sensor (optional vector),
-    #   oos_begin (vector), oos_end (vector)
-
-    # For trolls, we need site + plot + dates is all
-    # Troll o.o.s. knocks out entire group of sensors on it
-
-    # Read troll d.f.
-    # Missing times get 12:00
-    # Missing Date_replaced get MAX_DATE
-    # Date_replaced + 1 day (per)
-    #
-    # loop through troll d.f.
-    timestamps <- ymd_hms(c("2022-03-06 12:34:56", "2022-03-07 12:34:45", "2022-03-08 12:34:45", "2022-03-09 12:34:45"))
-    sensor <- "GW"
-    sites <- c("OWC", "OWC", "OWC", "OWC")
-    plots <- c("W", "W", "W", "W")
-
-    timestamp_dates <- as.Date(timestamps)
-
-    troll_flags <- rep(FALSE, length(timestamps))
-    for(i in seq_len(nrow(troll_oos))) {
-        troll_flags <- sensor == "GW" &
-            troll_flags | sites == oos_table$Site[i] &
-            plots == oos_table$Location[i] &
-            # per Peter R.: it takes 24 hours after troll replacement
-            # for a sensor to be considered 'good'
-            timestamp_dates >= oos_table$Date_pulled[i] &
-            timestamp_dates <= (oos_table$Date_replaced[1] + 1)
+    # Make sure that any 'extra' condition columns (in addition to the
+    # oos window begin and end) are present in the data d.f.
+    non_ts_fields <- setdiff(colnames(oos_df), c("oos_begin", "oos_end"))
+    if(!all(non_ts_fields %in% colnames(data_df))) {
+        stop("Not all out-of-service condition columns are present in data!")
     }
+    oos_final <- rep(FALSE, nrow(data_df))
 
-
-    troll_flags <- sites == troll$Site[i] &
-        timestamp_dates >= troll$Date_pulled[i] &
-        timestamp_dates <= (troll$Date_replaced[1] + 1)
-
-    # For Aquatroll, return
-    #   SITE == site &
-    #   sensor == sensor &
-    #   As.Date(timestamp) >= troll$Date_pulled &
-    #   as.Date(timestamp) <= troll$Date_replaced + 1 day
+    for(i in seq_len(nrow(oos_df))) {
+        # First check: are any timestamps within the oos window?
+        oos <- data_df$TIMESTAMP >= oos_df$oos_begin[i] &
+            data_df$TIMESTAMP <= oos_df$oos_end[i]
+        if(any(oos)) {
+            # There are timestamp matches, so check other (optional)
+            # conditions in the oos_df; they must match exactly
+            # For example, if there's a "Site" column it must
+            for(f in non_ts_fields) {
+                oos <- oos & data_df[,f] == oos_df[,f][i]
+            }
+            # The out-of-service flags for this row of the oos_df table
+            # are OR'd with the overall flags that will be returned below
+            oos_final <- oos_final | oos
+        }
+    }
+    return(oos_final)
 }
 
+# Test code for oos()
 
+data_df <- data.frame(TIMESTAMP = 1:3, x = letters[1:3], y = 4:6)
 
+# No other conditions beyond time window
+oos_df <- data.frame(oos_begin = 1, oos_end = 1)
+stopifnot(oos(oos_df, data_df) == c(TRUE, FALSE, FALSE))
+oos_df <- data.frame(oos_begin = 4, oos_end = 5)
+stopifnot(oos(oos_df, data_df) == c(FALSE, FALSE, FALSE))
+oos_df <- data.frame(oos_begin = 0, oos_end = 2)
+stopifnot(oos(oos_df, data_df) == c(TRUE, TRUE, FALSE))
+oos_df <- data.frame(oos_begin = 0, oos_end = 3)
+stopifnot(oos(oos_df, data_df) == c(TRUE, TRUE, TRUE))
+
+# x condition - doesn't match even though timestamp does
+oos_df <- data.frame(oos_begin = 1, oos_end = 1, x = "b")
+stopifnot(oos(oos_df, data_df) == c(FALSE, FALSE, FALSE))
+# x condition - matches and timestamp does
+oos_df <- data.frame(oos_begin = 1, oos_end = 1, x = "a")
+stopifnot(oos(oos_df, data_df) == c(TRUE, FALSE, FALSE))
+# x condition - some match, some don't
+oos_df <- data.frame(oos_begin = 1, oos_end = 2, x = "b")
+stopifnot(oos(oos_df, data_df) == c(FALSE, TRUE, FALSE))
+# x and y condition
+oos_df <- data.frame(oos_begin = 1, oos_end = 2, x = "b", y = 5)
+stopifnot(oos(oos_df, data_df) == c(FALSE, TRUE, FALSE))
+oos_df <- data.frame(oos_begin = 1, oos_end = 2, x = "a", y = 5)
+stopifnot(oos(oos_df, data_df) == c(FALSE, FALSE, FALSE))
+
+# Error thrown if condition column(s) not present
+oos_df <- data.frame(oos_begin = 1, oos_end = 2, z = 1)
+out <- try(oos(oos_df, data_df), silent = TRUE)
+stopifnot(class(out) == "try-error")
