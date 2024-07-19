6c25332400a403e0f5afcd913c7771a475422acf
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-07-19 03:50:40 -0400

    Update create-time-series.R

--- synoptic/data/L1/code_examples/create-time-series.R
+++ synoptic/data/L1/code_examples/create-time-series.R
@@ -1,18 +1,22 @@
 # Create a time series of a single variable at a site
+# This script assumes that the working directory is the "L1/" folder
+# of the COMPASS-FME L1 (Level 1) environmental sensor data
 
 site <- "CRC" # change this to e.g. "CRC_W" if you want a particular plot
 variable <- "soil_temp_10cm"
 
 # Construct a "regular expression" to find the files we want: in this case,
 # CSV files starting with the site code above
-pat <- paste0("^", site, ".*csv$")
+regex <- paste0("^", site, ".*csv$")
 
 # Get the names of the files we need. Note this assumes that your
 # working directory is the main directory of the L1 data
-files <- list.files("./", pattern = pat, recursive = TRUE, full.names = TRUE)
+files <- list.files("./", pattern = regex, recursive = TRUE, full.names = TRUE)
 
 # Helper function to read the files and filter to just the variable we want
-# We use readr::read_csv for easy timestamp handling
+# Use readr::read_csv for easy timestamp handling
+# Note that we set "col_types" to force the "Plot" column to be read as a
+# character; see https://github.com/COMPASS-DOE/data-workflows/issues/186
 library(readr)
 f <- function(f) {
     message("Reading ", basename(f))
