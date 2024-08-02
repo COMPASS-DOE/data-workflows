fc109de1a3b89ff2e20eab18a0043a7e0f2f7561
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-08-02 12:52:48 -0400

    Update cumulative-observations.R

--- synoptic/data/L1/code_examples/cumulative-observations.R
+++ synoptic/data/L1/code_examples/cumulative-observations.R
@@ -4,7 +4,8 @@
 # Find and count lines for all files in the L1 folder
 fls <- list.files("./data/L1/", pattern = "*.csv$", full.names = TRUE, recursive = TRUE)
 
-results <- data.frame(file = basename(fls), rows = NA_real_)
+library(tibble)
+results <- tibble(file = basename(fls), rows = NA_real_)
 
 for(i in seq_along(fls)) {
     message(basename(fls[i]))
@@ -17,9 +18,28 @@ library(tidyr)
 results <- separate(results, file, sep = "_", into = c("Site", "Plot", "Timerange","Level","version"))
 results <- separate(results, Timerange, sep = "-", into = c("Begin", "End"))
 results$Date <- as.Date(results$Begin, format = "%Y%m%d")
+results$Year <- year(results$Date)
+results$Month <- month(results$Date)
+
+# Make some graphs
+library(ggplot2)
+theme_set(theme_bw())
+library(scales)
+library(viridis)
+
+results %>%
+    group_by(Site, Year, Month) %>%
+    summarise(rows = sum(rows)) %>%
+    mutate(YearMonth = ymd(paste(Year, Month, "01"))) %>%
+    ggplot(aes(YearMonth, Site, fill = rows)) + geom_tile() +
+    xlab("Time") +
+    scale_fill_gradient("Observations", trans = scales::log_trans(base = 10),
+                        labels = unit_format(unit = "M", scale = 1e-6)) ->
+    p
+print(p)
+ggsave("~/Desktop/heatmap.png", height = 6, width = 10)
 
 # Compute cumulative observations by site and date
-library(dplyr)
 results %>%
     complete(Site, Date, fill = list(rows = 0)) %>%
     group_by(Site, Date) %>%
@@ -28,12 +48,6 @@ results %>%
     group_by(Site) %>%
     mutate(cum_n = cumsum(n)) ->
     smry
-
-# Make a nice graph
-library(ggplot2)
-theme_set(theme_bw())
-library(scales)
-library(viridis)
 
 p2 <- ggplot(smry, aes(Date, cum_n, fill = Site)) +
     geom_area(alpha = 0.8 , linewidth = 0.5, colour = "white") +
